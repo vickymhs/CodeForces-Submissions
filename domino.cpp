19bd2edc96c5558b1f266fb0a446e04a05e7f72b
vickymhs <43530800+vickymhs@users.noreply.github.com>
2019-10-30 13:11:25 +0530

    Round-47-A-Domino Piling

---
+++ domino.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int m,n,x,y;
+	cin>>m>>n;
+	
+	
+	cout<<(m*n)/2;
+	
+	/*if(m%2==0)
+	{
+		x=m/2;
+		cout<<x*y;	
+	}
+	else
+	{
+		x=m/2;
+		y=n/2;
+		cout<<x+y;
+	}
+	*/
+}
