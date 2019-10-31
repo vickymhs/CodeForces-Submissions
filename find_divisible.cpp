0e29f32612295bd1c6afeda8e45f3f22a4f0a475
vickymhs <43530800+vickymhs@users.noreply.github.com>
2019-10-31 15:39:38 +0530

    Educational-Round-57-A-Find divisible

---
+++ find_divisible.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long t,i,j,l,r;
+int solve()
+{
+	cin>>l>>r;
+	cout<<l<<" "<<(2*l)<<"\n";
+	return 0;
+}
+int main()
+{
+	cin>>t;
+	while(t--)
+	solve();
+}
