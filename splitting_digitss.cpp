beaaf5ccd20acc16277d021faa677090250d74e1
vickymhs <43530800+vickymhs@users.noreply.github.com>
2019-10-31 16:11:39 +0530

    Round-534-A-Splittimg into digits

---
+++ splitting_digitss.cpp
@@ -0,0 +1,15 @@
+	#include<bits/stdc++.h>
+	using namespace std;
+	int n,i,j;
+	//long long int
+	//string
+	#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	int main()
+	{
+		fastread;
+		cin>>n;
+		cout<<n<<endl;
+		for(i=0;i<n;i++)
+			cout<<1<<" ";
+		return 0;
+	}
